93a9a923cdac1e335c46c6925ede460c160715a7
Chris Burke <millions.of.stones@gmail.com>
2024-12-29 11:02:45 +0000

    fix base path

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://barrybingo.github.io/malarkey-of-the-new-worm/',
   build: {
     outDir: '../docs',
     emptyOutDir: true
